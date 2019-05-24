e99cb8456c104b223ff6073d202243488217dad4
levi-blodgett <levi_blodgett@outlook.com>
2019-05-24 09:21:16 -0500

    Fixed overflow for tooltips on active panels

--- accordian.js
+++ accordian.js
@@ -5,16 +5,16 @@ document.getElementById("body").className += " load";
 var acc = document.getElementsByClassName("accordion");
 
 for (var i = 0; i < acc.length; i++) {
+  // Add event to enable preview of div when mousing into the accordian
   acc[i].addEventListener("mouseenter", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     var description = this.nextElementSibling.children[0];
-    if (panel.style.maxHeight === panel.scrollHeight + "px") {
-      
-    } else {
+    if (panel.style.maxHeight != panel.scrollHeight + "px") {
       panel.style.maxHeight = description.scrollHeight + "px";
     }
   });
+  // Add event to disable preview of div when mousing out of accordian
   acc[i].addEventListener("mouseleave", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
@@ -23,6 +23,7 @@ for (var i = 0; i < acc.length; i++) {
       panel.style.maxHeight = null;
     }
   });
+  // Add event to make panels active and bring them to max height
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     this.scrollIntoView(true);
@@ -32,5 +33,16 @@ for (var i = 0; i < acc.length; i++) {
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
     } 
+  });
+  // Add event to make it so overflow doesn't affect active panels
+  // Hides overflow when hiding the panel
+  acc[i].addEventListener("mouseup", function() {
+    if (this.classList.contains("active")) {
+      this.nextElementSibling.style.overflow = "visible";
+      console.log('yes');
+    } else {
+      console.log('no');
+      this.nextElementSibling.style.overflow = "hidden";
+    }
   });
 }
